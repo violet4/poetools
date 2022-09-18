497ca9db06f15bf6f30ab033de4bf33a78dfaf17
violet eldridge <violet@violet-mica.com>
2022-09-17 19:50:51 -0700

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,144 @@
+import logo from './logo.svg';
+import './App.css';
+import {useState} from 'react';
+
+const hidden = {
+  "Mirror Shard": 2141.205,
+  "Tempering Orb": 624.3,
+  "Sacred Crystallised Lifeforce": 313.5,
+  "Secondary Regrading Lens": 209.0,
+  "Tailoring Orb": 209.0,
+  "Tainted Divine Teardrop": 208.7,
+  "Orb of Dominance": 180.0,
+  "Hunter's Exalted Orb": 134.0,
+  "Awakener's Orb": 94.0,
+  "Tainted Exalted Orb": 69.0,
+  "Tainted Chaos Orb": 68.64285714285714,
+  "Prime Regrading Lens": 56.666666666666664,
+  "Redeemer's Exalted Orb": 40.0,
+  "Crusader's Exalted Orb": 40.0,
+  "Warlord's Exalted Orb": 40.0,
+  "Orb of Conflict": 29.85774,
+  "Eldritch Chaos Orb": 27.473684210526315,
+  "Exceptional Eldritch Ichor": 23.7,
+  "Tainted Mythic Orb": 23.176470588235293,
+  "Blessing of Chayula": 22.0,
+  "Eldritch Orb of Annulment": 18.0,
+  "Otherworldly Scouting Report": 14.0,
+  "Exceptional Eldritch Ember": 11.461539,
+  "Comprehensive Scouting Report": 11.5,
+  "Charged Compass": 10.857142857142858,
+  "Blessing of Uul-Netol": 10.0,
+  "Unstable Catalyst": 10.0,
+  "Tainted Blessing": 10.0,
+  "Influenced Scouting Report": 9.851851111111111,
+  "Eldritch Exalted Orb": 9.0,
+  "Blessing of Esh": 7.368421052631579,
+  "Blessing of Tul": 7.0,
+  "Tainted Orb of Fusing": 6.0,
+  "Blessing of Xoph": 5.0,
+  "Fertile Catalyst": 5.002008333333333,
+  "Singular Scouting Report": 4.726815263157895,
+  "Awakened Sextant": 3.21442,
+  "Grand Eldritch Ichor": 2.9267625,
+  "Veiled Chaos Orb": 0.1571425,
+  "Accelerating Catalyst": 2.482325,
+  "Delirious Scouting Report": 2.125,
+  "Blighted Scouting Report": 2.0,
+  "Grand Eldritch Ember": 2.0,
+  "Tainted Jeweller's Orb": 1.88889,
+  "Harbinger's Orb": 1.3328795,
+  "Greater Eldritch Ichor": 1.3092882352941178,
+  "Greater Eldritch Ember": 0.975,
+  "Exalted Shard": 1.0,
+  "Tempering Catalyst": 1.0049,
+  "Tainted Armourer's Scrap": 0.9878569230769231,
+  "Vaal Scouting Report": 0.865225294117647,
+  "Lesser Eldritch Ember": 0.9056878571428572,
+  "Explorer's Scouting Report": 0.803225,
+  "Intrinsic Catalyst": 0.71429,
+  "Noxious Catalyst": 0.5689257142857143,
+  "Tainted Chromatic Orb": 0.33333,
+  "Lesser Eldritch Ichor": 0.5264916666666667,
+  "Turbulent Catalyst": 0.33479846153846154,
+  "Tainted Blacksmith's Whetstone": 0.32854666666666665,
+  "Imbued Catalyst": 0.31216571428571427,
+  "Abrasive Catalyst": 0.30441,
+  "Vivid Crystallised Lifeforce": 0.056639,
+  "Wild Crystallised Lifeforce": 0.03553,
+  "Primal Crystallised Lifeforce": 0.02496  
+};
+
+const data = {
+  "Mirror of Kalandra": 41743.0,
+  "Divine Orb": 209.0,
+  "Sacred Orb": 32.666666666666664,
+  "Elevated Sextant": 29.85774,
+  "Exalted Orb": 17.41597,
+  "Prismatic Catalyst": 9.506777777777778,
+  "Orb of Annulment": 8.986015384615385,
+  "Ritual Vessel": 5.0,
+  "Ancient Orb": 0.5826728571428571,
+  "Operative's Scouting Report": 3.4,
+  "Surveyor's Compass": 1.6228094736842105,
+  "Orb of Unmaking": 1.338208,
+  "Gemcutter's Prism": 1.1944466666666667,
+  "Stacked Deck": 1.0,
+  "Chaos Orb": 1,
+  "Annulment Shard": 0.9803933333333333,
+  "Regal Orb": 0.7095266666666666,
+  "Oil Extractor": 0.64285625,
+  "Orb of Regret": 0.511515,
+  "Vaal Orb": 0.528036,
+  "Enkindling Orb": 0.47619,
+  "Blessed Orb": 0.475515,
+  "Instilling Orb": 0.4495955555555556,
+  "Orb of Scouring": 0.35281090909090906,
+  "Orb of Horizons": 0.218894375,
+  "Orb of Alteration": 0.18555444444444444,
+  "Orb of Fusing": 0.16369666666666666,
+  "Chromatic Orb": 0.13127285714285714,
+  "Glassblower's Bauble": 0.13333,
+  "Cartographer's Chisel": 0.11111,
+  "Engineer's Orb": 0.11244105263157894,
+  "Orb of Alchemy": 0.09091,
+  "Orb of Binding": 0.07533882352941176,
+  "Orb of Chance": 0.065077,
+  "Orb of Augmentation": 0.057066,
+  "Jeweller's Orb": 0.05458,
+  "Portal Scroll": 0.01667,
+  "Orb of Transmutation": 0.01063,
+  "Scroll of Wisdom": 0.006165333333333333,
+  "Blacksmith's Whetstone": 0.006413846153846154
+};
+
+function App() {
+  const [denom, setDenom] = useState(1);
+  return (
+    <div className="App">
+      {/* {Object.entries(data).forEach(([a, b]) => <span>a</span>)} */}
+      {/* {Object.entries(data).forEach(([key, value]) => console.log(`${key}: ${value}`))} */}
+      <table>
+        {Object.entries(data).sort(([k,v]) => -v).map(([key, value]) =>
+          <tr onClick={e => setDenom(value)}>
+            <td>
+              {key}
+            </td>
+            <td
+              style={{textAlign: 'right'}}
+            >
+              {(value / denom).toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+            </td>
+            <td
+              style={{textAlign: 'right'}}
+            >
+              {(denom / value).toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+            </td>
+          </tr>
+        )}
+      </table>
+    </div>
+  );
+}
+
+export default App;
---
+++ poe.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python3
+"""
+
+https://www.craftofexile.com/
+
+
+User-Agent: OAuth {$clientId}/{$version} (contact: {$contact}) ...
+
+Example:
+
+User-Agent: OAuth mypoeapp/1.0.0 (contact: mypoeapp@gmail.com) StrictMode
+
+
+This product isn't affiliated with or endorsed by Grinding Gear Games in any way.
+
+Code 	Text 	Description
+200	OK	The request succeeded.
+202	Accepted	The request was accepted but may not be processed yet.
+400	Bad Request	The request was invalid. Check that all arguments are in the correct format.
+404	Not Found	The requested resource was not found.
+429	Too many requests	You are making too many API requests and have been rate limited.
+500	Internal Server Error	We had a problem processing your request. Please try again later or post a bug report.
+
+
+{
+    "error": {
+        "code": 2,
+        "message": "Invalid query"
+    }
+}
+
+
+Examples:
+Code 	Message
+0	Accepted
+1	Resource not found
+2	Invalid query
+3	Rate limit exceeded
+4	Internal error
+5	Unexpected content type
+8	Unauthorized
+6	Forbidden
+7	Temporarily Unavailable
+9	Method not allowed
+10	Unprocessable Entity
+
+
+
+Invalid Requests Threshold
+Applications (and users) that make too many invalid requests in a short period of time will be restricted from further access to our service.
+Invalid requests include any response codes in the HTTP 4xx range. This includes common codes such as 401 (Unauthorized), 403 (Forbidden), and 429 (Too Many Requests).
+Reasonable attempts must be made in order to avoid passing the threshold. 
+
+
+
+HTTP/1.1 200 OK
+...
+X-Rate-Limit-Policy: ladder-view
+X-Rate-Limit-Rules: client
+X-Rate-Limit-Client: 10:5:10
+X-Rate-Limit-Client-State: 1:5:0
+
+Example that has exceeded the limit:
+
+HTTP/1.1 429 Too Many Requests
+...
+X-Rate-Limit-Policy: ladder-view
+X-Rate-Limit-Rules: client
+X-Rate-Limit-Client: 10:5:10
+X-Rate-Limit-Client-State: 11:5:10
+Retry-After: 10
+Retry-After
+    Time to wait (in seconds) until the rate limit expires.
+
+
+
+Registration is currently handled by us directly at our discretion. You can make a request by emailing oauth@grindinggear.com with your details and requirements.
+Things to include:
+
+    Your PoE account name.
+    Your application's name.
+    The scopes you are interested in.
+    A secure redirect URI if you plan to use the authorization_code grant type.
+
+
+https://www.pathofexile.com/my-account/applications
+
+account:stashes
+account:characters
+service:psapi
+account:league_accounts - allocated atlas passives
+account:item_filter
+
+
+Application Credentials
+
+Once your application has been registered you will be able to find your credentials by clicking "Manage" on your application here. If this is your first time visiting this page you will need to generate your client_secret and copy it down somewhere safe. If you lose your current client_secret you can always regenerate another
+
+https://www.pathofexile.com/my-account/applications
+https://www.pathofexile.com/developer/docs#guidelines
+https:/https://www.pathofexile.com/developer/docs/authorization#scopes/www.pathofexile.com/developer/docs/authorization#scopes
+https://www.pathofexile.com/developer/docs/authorization#scopes
+https://www.pathofexile.com/developer/docs/authorization#oauth
+https://www.pathofexile.com/developer/docs/reference
+https://www.pathofexile.com/developer/docs#guidelines
+
+
+"""
+import datetime
+import os
+import json
+
+import requests
+
+today = datetime.date.today().strftime('%Y-%m-%d')
+filename = f'currency_{today}.txt'
+if not os.path.exists(filename):
+    resp = requests.get('https://poe.ninja/api/data/CurrencyOverview?league=Kalandra&type=Currency&language=en')
+    with open(filename, 'w') as fw:
+        print(resp.content.decode(), file=fw)
+
+with open(filename, 'r') as fr:
+    data = json.load(fr)
+
+# import ipdb; ipdb.set_trace()
+currency = dict()
+for cur in data['lines']:
+    currencyTypeName = cur['currencyTypeName']
+    if 'receive' not in cur:
+        print(f"no data for {currencyTypeName}")
+        continue
+    value = cur['receive']['value']
+    currency[currencyTypeName] = value
+
+
+print(json.dumps(currency, indent=4))
+print({k: v for k, v in currency.items() if 'chaos' in k.lower()})
+
+
